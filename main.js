fe844eaf3fbc48ce3058a2807a5935139cfe6d15
Srdjan Milic <srdjanrmilic@gmail.com>
2018-08-24 23:47:16 +0200

    eslint checks

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const { app, BrowserWindow } = require('electron');
+const {
+  app,
+  BrowserWindow
+} = require('electron');
 require('electron-context-menu')({
-	prepend: (params) => [{
-		label: 'Rainbow',
-		// Only show it when right-clicking images
-		visible: params.mediaType === 'image'
-	}]
+  prepend: (params) => [{
+    label: 'Rainbow',
+    // Only show it when right-clicking images
+    visible: params.mediaType === 'image'
+  }]
 });
 
 
@@ -13,26 +16,32 @@ require('electron-context-menu')({
 let win;
 
 function createWindow() {
-	// Create the browser window.
-	win = new BrowserWindow({width: 1150, height: 780, show: false, title: 'Google Translate', icon: 'images/google-translate.png'})
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1150,
+    height: 800,
+    show: false,
+    title: 'Google Translate',
+    icon: 'images/google-translate.png'
+  })
 
-	// and load the index.html of the app.
-	win.loadURL('https://translate.google.com')
+  // and load the index.html of the app.
+  win.loadURL('https://translate.google.com')
 
-	// Open the DevTools.
-	// win.webContents.openDevTools()
+  // Open the DevTools.
+  // win.webContents.openDevTools()
 
-	// Emitted when the window is closed.
-	win.on('closed', () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		win = null
-	});
-	
-	win.once('ready-to-show', () => {
-		win.show();
-	});
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  });
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 }
 
 // This method will be called when Electron has finished
@@ -43,11 +52,11 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-	// On macOS it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
